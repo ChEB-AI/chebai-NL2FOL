5f1d6a6698ca2e6effd83606ee31b2cc2c55e7f8
aditya0by0 <aditya0by0@gmail.com>
2025-12-25 14:17:58 +0100

    use load_env + add tracebility

--- nl_2_fol/prompting/__init__.py
+++ nl_2_fol/prompting/__init__.py
@@ -1,41 +1,28 @@
 import os
 
+from dotenv import load_dotenv
+
 GROQ_API_KEY_NAME = "GROQ_API_KEY"
 OPENAI_API_KEY_NAME = "OPENAI_API_KEY"
-
 API_KEYS_NAME_LIST = [GROQ_API_KEY_NAME, OPENAI_API_KEY_NAME]
 
 
-def set_api_env_var(file_path="./api_keys.env"):
-    # Get groq api key from : https://console.groq.com/keys
-    # Get openai api key from: https://openai.com/api/
-
-    # if tracing needed in future
-    # os.environ['LANGCHAIN_TRACING_V2'] = 'true'
-    # os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
-    # assert "LANGCHAIN_API_KEY" in os.environ
-
-    flag_api_key_set = False
-    # Read the file and set environment variables
-    with open(file_path, "r") as f:
-        for line in f:
-            if not line.strip() or line.startswith("#"):
-                continue
-            # Split key and value
-            key, value = line.strip().split("=", 1)
-            if value is None or value.strip() == "":
-                raise ValueError(f"Invalid line in {file_path}: {line}")
-            # Set environment variable
-            if key in API_KEYS_NAME_LIST:
-                os.environ[key] = value
-                print(f"Environment variable set: {key}")
-                flag_api_key_set = True
-
-    if not flag_api_key_set:
-        raise ValueError(
-            f"No valid api key found in {file_path}\n",
-            f"Use of the following api keys: {API_KEYS_NAME_LIST}",
-        )
+load_dotenv("./api_keys.env")  # This loads the .env file
+# Get groq api key from : https://console.groq.com/keys
+# Get openai api key from: https://openai.com/api/
 
 
-set_api_env_var()
+assert any(os.getenv(key) for key in API_KEYS_NAME_LIST), (
+    f"Detailed Error: None of the required API keys ({API_KEYS_NAME_LIST}) "
+    "were found in the environment variables. Please check your .env file."
+)
+
+
+tracing = True
+if tracing:
+    # Refer Tracibility docs: https://docs.langchain.com/langsmith/observability-quickstart
+    assert (
+        "LANGSMITH_TRACING" in os.environ and os.environ["LANGSMITH_TRACING"] == "true"
+    )
+    assert "LANGSMITH_WORKSPACE_ID" in os.environ  # Not necessary, but good to have
+    assert "LANGSMITH_API_KEY" in os.environ
