9dc903691a8c17003ca5f41bd4052ac7be743162
aditya0by0 <aditya0by0@gmail.com>
2026-03-10 14:51:45 +0100

    add langsmith project

--- nl_2_fol/prompting/__init__.py
+++ nl_2_fol/prompting/__init__.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 GROQ_API_KEY_NAME = "GROQ_API_KEY"
 OPENAI_API_KEY_NAME = "OPENAI_API_KEY"
 ANTHROPIC_API_KEY = "ANTHROPIC_API_KEY"
-API_KEYS_NAME_LIST = [GROQ_API_KEY_NAME, OPENAI_API_KEY_NAME]
+API_KEYS_NAME_LIST = [GROQ_API_KEY_NAME, OPENAI_API_KEY_NAME, ANTHROPIC_API_KEY]
 
 
 load_dotenv("./api_keys.env")  # This loads the .env file
@@ -24,8 +24,22 @@ if tracing:
     # Refer Tracibility docs: https://docs.langchain.com/langsmith/observability-quickstart
     # View on trace of the prompting here:
     # https://smith.langchain.com/public/5dddca10-2d31-4be8-b4c3-caca98504868/r/019b55af-4679-7e31-96f0-516ffad9499d
+
+    # LANGSMITH_TRACING=true
+    # LANGSMITH_PROJECT="nl2fol"
+    # LANGSMITH_API_KEY=<your-api-key>
+    # LANGSMITH_ENDPOINT=https://api.smith.langchain.com
+
     assert (
         "LANGSMITH_TRACING" in os.environ and os.environ["LANGSMITH_TRACING"] == "true"
     )
     # assert "LANGSMITH_WORKSPACE_ID" in os.environ  # Not necessary, but good to have
     assert "LANGSMITH_API_KEY" in os.environ
+    assert (
+        "LANGSMITH_ENDPOINT" in os.environ
+        and os.environ["LANGSMITH_ENDPOINT"] == "https://api.smith.langchain.com"
+    )
+    assert (
+        "LANGSMITH_PROJECT" in os.environ
+        and os.environ["LANGSMITH_PROJECT"] == "nl2fol"
+    )
