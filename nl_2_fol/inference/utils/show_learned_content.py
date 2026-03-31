61f37b6066b050d7ffad02fa5734dab45d16cbd3
aditya0by0 <aditya0by0@gmail.com>
2026-03-31 16:47:48 +0200

    print add def related to class if any

--- nl_2_fol/inference/utils/show_learned_content.py
+++ nl_2_fol/inference/utils/show_learned_content.py
@@ -32,6 +32,13 @@ def print_pickle_contents(
         print(f"Metrics: {learned_def.train_metrics}")
         print(f"Formula: {learned_def.learned_FOL.formula}")
         print(f"Learned success: {learned_def.learn_success}")
+
+        if learned_def.additional_defs_used:
+            print("Additional definitions used:")
+            for name, (vars, add_def) in learned_def.additional_defs_used.items():
+                print(
+                    f"  {name} with variables {[str(var) for var in vars]} and formula: {add_def.formula}"
+                )
         if show_system_prompt:
             print("System prompt:")
             print(learned_def.prompts_history["system_prompt"])
