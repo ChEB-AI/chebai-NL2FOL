61fc01067a32d027fe2aebb68e53f5d09fff0e67
aditya0by0 <aditya0by0@gmail.com>
2026-06-11 14:08:42 +0200

    add classes to validate code

--- nl_2_fol/inference/utils/print_all_defs.py
+++ nl_2_fol/inference/utils/print_all_defs.py
@@ -64,9 +64,30 @@ def print_all_fols(pickle_file_path: Path, split="train"):
     print(f"Total main formulas printed for split '{split}': {main_formulas_counter}")
 
 
+def check_number_of_classes_to_validate(pickle_file_path: Path):
+    with open(pickle_file_path, "rb") as f:
+        data: DefinitionLearningResults = pickle.load(f)
+
+    classes = []
+    with open("classes_4.txt", "r") as f:
+        classes = [line.strip() for line in f]
+
+    classes_to_validate = [
+        learned_def.name
+        for _, learned_def in data.learned_definitions.items()
+        if learned_def.learn_success
+        and learned_def.val_metrics is None
+        and learned_def.name in classes
+    ]
+
+    [print(f"{class_name}") for class_name in classes_to_validate]
+    print(f"Classes to validate: {len(classes_to_validate)}")
+
+
 if __name__ == "__main__":
     pickle_file_path = Path(
-        "nl_2_fol/inference/learner/learned/claude-opus-4-6/v1_learned_definitions_a3_with_val.pkl"
+        "nl_2_fol/inference/learner/learned/claude-opus-4-6/learned_definitions_a3_with_val.pkl"
     )
-    print_all_fols(pickle_file_path=pickle_file_path, split="val")
+    check_number_of_classes_to_validate(pickle_file_path=pickle_file_path)
+    # print_all_fols(pickle_file_path=pickle_file_path, split="val")
     # For Val: Total main formulas printed for split 'val': 198
