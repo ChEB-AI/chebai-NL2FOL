82b3778241a2a95c73b0e42a7630e7a2c9f95a76
aditya0by0 <aditya0by0@gmail.com>
2026-06-12 12:38:46 +0200

    comment out mistral parser

--- nl_2_fol/inference/fol_reasoner/__init__.py
+++ nl_2_fol/inference/fol_reasoner/__init__.py
@@ -1,4 +1,7 @@
-from .finetuned_mistral_reasoner.mistral_fol_to_tptp import MistralCustomFOLReasoner
+# from .finetuned_mistral_reasoner.mistral_fol_to_tptp import MistralCustomFOLReasoner
 from .model_check_molecule import GavelFOLReasoner
 
-__all__ = ["GavelFOLReasoner", "MistralCustomFOLReasoner"]
+__all__ = [
+    "GavelFOLReasoner",
+    #    "MistralCustomFOLReasoner"
+]
