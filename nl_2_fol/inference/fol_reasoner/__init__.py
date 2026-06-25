a472394cbd391147eeb67b9828ceac82b8d602fd
aditya0by0 <aditya0by0@gmail.com>
2026-06-26 00:08:36 +0200

    rename ChemlogFOLDefinition TO ChemlogDefinition

--- nl_2_fol/inference/fol_reasoner/__init__.py
+++ nl_2_fol/inference/fol_reasoner/__init__.py
@@ -1,11 +1,11 @@
 # from .finetuned_mistral_reasoner.mistral_fol_to_tptp import MistralCustomFOLReasoner
 from .asp_model_checker import ASPDefinition, ASPModelChecker
 from .base import FOLDefinition
-from .chemlog_model_checker import ChemlogFOLDefinition, ChemlogModelChecker
+from .chemlog_model_checker import ChemlogDefinition, ChemlogModelChecker
 
 __all__ = [
     "ChemlogModelChecker",
-    "ChemlogFOLDefinition",
+    "ChemlogDefinition",
     "ASPModelChecker",
     "ASPDefinition",
     "FOLDefinition",
