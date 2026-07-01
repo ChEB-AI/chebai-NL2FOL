3f5cab2c91ef9c7e59e911f960d2c264c6e9217d
aditya0b0 <aditya0by0@gmail.com>
2026-07-01 11:44:24 +0200

    fix manual test path

--- _manual_tests/_mTeesstt_predicate_retrieval.py
+++ _manual_tests/_mTeesstt_predicate_retrieval.py
@@ -4,7 +4,7 @@ from nl_2_fol.prompting.retrieve_relevant_predicates import (
     SemanticPredicateRetriever,
 )
 
-with open(os.path.join("tests", "_manual_tests", "predicates_list.txt"), "r") as f:
+with open(os.path.join("_manual_tests", "predicates_list.txt"), "r") as f:
     predicates = [line.strip() for line in f.readlines()]
 
 
@@ -13,9 +13,19 @@ retriever.add_predicates(predicates)
 
 
 definition = """
-A carbon oxoacid acid [sic] carrying at least one ‒C(=O)OH group and having the
- structure RC(=O)OH, where R is any any monovalent functional group.
- Carboxylic acids are the most common type of organic acid.
+CHEBI:33575 - carboxylic acid : A carbon oxoacid acid carrying at least one ‒C(=O)OH group and having the structure RC(=O)OH, where R is any any monovalent functional group. Carboxylic acids are the most common type of organic acid.
+
+Outgoing Relation(s)
+
+carboxylic acid (CHEBI:33575) has part carboxy group (CHEBI:46883)
+
+carboxylic acid (CHEBI:33575) is a carbon oxoacid (CHEBI:35605)
+
+carboxylic acid (CHEBI:33575) is a carbonyl compound (CHEBI:36586)
+
+carboxylic acid (CHEBI:33575) is a organic acid (CHEBI:64709)
+
+carboxylic acid (CHEBI:33575) is conjugate acid of carboxylic acid anion (CHEBI:29067)
 """
 
 matches = retriever.retrieve_relevant_predicates(definition, top_k=None, threshold=0.35)
